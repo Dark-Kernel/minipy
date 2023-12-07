0cd3026e80ddf6376a8d733f724391ddffa91eb4
Dark-Kernel <Mr-SimpleEditor@protonmail.com>
2023-12-07 20:42:46 +0530

    Implemented fernet algorithm.

--- src/app.py
+++ src/app.py
@@ -1,6 +1,7 @@
 from flask import Flask, request, send_file
 import io
 from src.crypto_functions.rsa_handler import rsa_encrypt, rsa_decrypt
+from src.crypto_functions.fernet_handler import fernet_encrypt, fernet_decrypt
 
 app = Flask(__name__)
 
@@ -11,25 +12,42 @@ def start():
 
 @app.route('/encr', methods=['GET', 'POST'])
 def receive():
+    algorithm = f"{request.args.get('algo')}"
+    print(algorithm)
     secret_pass = request.form.get("pass")
     plain_file = request.files.get("file")
     if plain_file and plain_file.filename:
-        zip_buffer = rsa_encrypt(plain_file, secret_pass)
-        return send_file(zip_buffer, mimetype='application/zip', as_attachment=True, download_name='encrypted.zip')          
+        if algorithm == 'rsa':
+            print("---------------rsa-------------")
+            zip_buffer = rsa_encrypt(plain_file, secret_pass)
+            return send_file(zip_buffer, mimetype='application/zip', as_attachment=True, download_name='encrypted.zip')          
+        elif algorithm == 'fernet':
+            zip_buffer = fernet_encrypt(plain_file, secret_pass)
+            return send_file(zip_buffer, mimetype='application/zip', as_attachment=True, download_name='encrypted.zip')          
+        else: 
+            return "Invalidddd"
+            
     else:
         return 'invalid file'
     
 
 @app.route('/decr', methods=['GET', 'POST'])
 def send_decrypt():
+    algorithm = request.args.get("algo").lower()
     secret_pass = request.form.get("pass")
     cipher_file = request.files.get("cipher")
     private_key_file = request.files.get("privkey")
     if cipher_file and cipher_file.filename and private_key_file and private_key_file.filename:
-        decrypted_text = rsa_decrypt(cipher_file, private_key_file, secret_pass)
-        response = send_file(io.BytesIO(decrypted_text), mimetype='text/plain', as_attachment=True, download_name='decrypted.txt')
-        response.headers.add('Content-Disposition', 'attachment')
-        return response
+        if algorithm == 'rsa':
+            decrypted_text = rsa_decrypt(cipher_file, private_key_file, secret_pass)
+            response = send_file(io.BytesIO(decrypted_text), mimetype='text/plain', as_attachment=True, download_name='decrypted.txt')
+            response.headers.add('Content-Disposition', 'attachment')
+            return response
+        elif algorithm == 'fernet':
+            decrypted_text = fernet_decrypt(cipher_file, private_key_file, secret_pass)
+            response = send_file(io.BytesIO(decrypted_text), mimetype='text/plain', as_attachment=True, download_name='decrypted.txt')
+            response.headers.add('Content-Disposition', 'attachment')
+            return response
     else:
         return "invalid file"       
 
---
+++ src/crypto_functions/fernet_handler.py
@@ -0,0 +1,47 @@
+from cryptography.fernet import Fernet
+from cryptography.hazmat.primitives import hashes
+from cryptography.hazmat.primitives.kdf.pbkdf2 import PBKDF2HMAC
+from src.utils.common_utils import read_file_content, create_zip_buffer
+import base64, os
+
+
+def fernet_encrypt(plain_file, secret_pass):
+    if not secret_pass:
+        secret_pass = "somepass"
+    if plain_file and plain_file.filename:
+        plain_data = read_file_content(plain_file)
+        salt = os.urandom(16)
+        
+        kdf = PBKDF2HMAC(
+            algorithm=hashes.SHA256(),
+            length=32,
+            salt=salt,
+            iterations=480000
+        )
+
+        key = base64.urlsafe_b64encode(kdf.derive(secret_pass.encode('utf-8'))) # key = Fernet.generate_key()
+        f = Fernet(key)
+        cipher_data = f.encrypt(plain_data.getvalue())
+         
+        files_dict = {
+            'cipher.encr': cipher_data,
+            'key.pem': key
+        }
+
+        return create_zip_buffer(files_dict)
+    else:
+        return "invalid file"
+
+
+    
+def fernet_decrypt(cipher_file, key_file, secret_pass=None):
+    if cipher_file and cipher_file.filename:
+        cipher_data = read_file_content(cipher_file)
+        key_data = read_file_content(key_file)
+        f = Fernet(key_data.getvalue())
+        plain_data = f.decrypt(cipher_data.getvalue())
+        return plain_data
+    else:
+        return "invalid file"
+        
+        
